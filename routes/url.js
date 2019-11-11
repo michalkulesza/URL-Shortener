53a612cbdd55311ff6dd9253158cf64d6ac18b09
xxcxxz <kulesza.michal@gmail.com>
2019-11-11 23:46:14 +0100

    Removed url verification

--- routes/url.js
+++ routes/url.js
@@ -12,40 +12,31 @@ router.post("/shorten", async (req, res) => {
   const { longUrl } = req.body;
   const baseUrl = config.get("baseUrl");
 
-  //Check base url
-  if (!vaildUrl.isUri(baseUrl)) {
-    return res.status(401).json("Invalid base URL");
-  }
-
   //Generate short url
   const urlCode = shortid.generate();
 
   //Check long url
-  if (vaildUrl.isUri(longUrl)) {
-    try {
-      let url = await Url.findOne({ longUrl });
+  try {
+    let url = await Url.findOne({ longUrl });
 
-      if (url) {
-        res.json(url);
-      } else {
-        const shortUrl = baseUrl + "/" + urlCode;
+    if (url) {
+      res.json(url);
+    } else {
+      const shortUrl = baseUrl + "/" + urlCode;
 
-        url = new Url({
-          longUrl,
-          shortUrl,
-          urlCode,
-          date: new Date()
-        });
+      url = new Url({
+        longUrl,
+        shortUrl,
+        urlCode,
+        date: new Date()
+      });
 
-        await url.save();
-        res.json(url);
-      }
-    } catch (err) {
-      console.log(err);
-      res.status(500).json("Server error");
+      await url.save();
+      res.json(url);
     }
-  } else {
-    res.status(401).json("Invalid long URL");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json("Server error");
   }
 });
 
