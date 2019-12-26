752a0ca4daaa98c3a6a7d7ed50666615bfcdd166
xxcxxz <kulesza.michal@gmail.com>
2019-12-26 13:36:12 +0100

    CORS fixes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ connectDB();
 
 app.use(express.json());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://michalkulesza.me");
+  res.header("Access-Control-Allow-Origin", "https://michalkulesza.me");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
